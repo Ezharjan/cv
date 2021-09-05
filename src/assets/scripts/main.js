4189d8fdd38133bb5ecc984604686bfd5741754e
Ezharjan <ezharjan@aliyun.com>
2021-09-05 20:47:56 +0800

    -

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -2,22 +2,22 @@ import 'focus-visible'
 
 document.documentElement.classList.remove('no-js')
 
-document.documentElement.classList.add(scrollClassName)
 // Scroll State
-const onScroll = () => {
+// const onScroll = () => {
+const AlwaysOpen = () => {
     const scrollClassName = 'js-scrolled'
     const scrollTreshold = 200
     const isOverTreshold = window.scrollY > scrollTreshold
     
-    // document.documentElement.classList.add(scrollClassName)
+    document.documentElement.classList.add(scrollClassName)
     // if (isOverTreshold) {
     //     document.documentElement.classList.add(scrollClassName)
     // } else {
     //     document.documentElement.classList.remove(scrollClassName)
     // }
 }
-window.addEventListener('scroll', onScroll, { passive: true })
-
+// window.addEventListener('scroll', onScroll, { passive: true })
+AlwaysOpen()
 // Print Button
 const printButton = document.querySelector('.js-print')
 printButton.addEventListener('click', () => {
