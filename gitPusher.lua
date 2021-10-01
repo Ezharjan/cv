186ad73f3af7715c86bfdd753727a94ac262bcd7
Ezharjan <ezharjan@aliyun.com>
2021-10-01 23:51:23 +0800

    renewed

--- gitPusher.lua
+++ gitPusher.lua
@@ -18,7 +18,7 @@ function tryTillSucceed(arg,tryTimes)
 end
 
 local pushCmd =  'git push -u origin built'
-local pushCmdGitee =  'git push -u originGitee built'
+local pushCmdGitee =  'git push -f originGitee built'
 local pullCmd =  'git pull'
 
 commander('git add .')
